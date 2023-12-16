5933ff42d9149a521b2a671a3e6094ec97c8a60f
Gentil Alyson Jácome Fernandes <fernandes.gentilalysonjac@gmail.com>
2023-12-16 18:56:50 -0300

    refactor: changes challenge3 to use BaseHashMapManager

--- challenge3.py
+++ challenge3.py
@@ -1,3 +1,6 @@
+from classes import BaseHashMapManager
+
+
 class Sensor:
     def __init__(self, id: int, read_value: float) -> None:
         self.__id = id
@@ -23,25 +26,8 @@ class Sensor:
         return f"Sensor ID: {self.id} - {self.read_value}"
 
 
-class SensorsSet:
-    def __init__(self):
-        self.__sensors_set = {}
-
-    def add_sensor(self, sensor: Sensor) -> None:
-        self.__sensors_set[sensor.id] = sensor
-
-    def find_sensor(self, id: int) -> Sensor|None:
-        try:
-            return self.__sensors_set[id]
-        except KeyError:
-            return None
-    
-    def update_sensor(self, id: int, new_read_value: float) -> bool:
-        sensor = self.find_sensor(id)
-        if sensor:
-            sensor.read_value = new_read_value
-            return True
-        return False
+class SensorsSet(BaseHashMapManager):
+    pass
 
 
 sensor_one = Sensor(1, 35.6)
@@ -49,10 +35,10 @@ sensor_two = Sensor(2, 59.3)
 sensor_three = Sensor(3, 12.0)
 sensor_set = SensorsSet()
 
-print(sensor_set.find_sensor(4))
-sensor_set.add_sensor(sensor_one)
-sensor_set.add_sensor(sensor_two)
-sensor_set.add_sensor(sensor_three)
-print(sensor_set.find_sensor(3))
-print(sensor_set.update_sensor(2, 60))
-print(sensor_set.find_sensor(2))
+print(sensor_set.find_item(4))
+sensor_set.add_item(sensor_one.id, sensor_one)
+sensor_set.add_item(sensor_two.id, sensor_two)
+sensor_set.add_item(sensor_three.id, sensor_three)
+print(sensor_set.find_item(3))
+print(sensor_set.update_item(2, read_value=60))
+print(sensor_set.find_item(2))
